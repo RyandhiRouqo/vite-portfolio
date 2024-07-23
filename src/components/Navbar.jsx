dc8585e0024c78b67fc120559b815ea6bf1ffc4d
RyandhiRouqo <ryanselifan@gmail.com>
2024-07-24 00:40:52 +0700

    "test"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
       <div className="absolute bg-black inset-0 w-full mx-auto min-h-14"></div>
       <nav className="sticky inset-0 z-50 bg-black/10 backdrop-blur-lg flex justify-between items-center w-full mx-auto min-h-14">
         <div className="ml-10">
-          <p className="font-bold text-rose-600 font-sans">ROUQO</p>
+          <p className="font-bold text-blue-600 font-sans">ROUQO</p>
         </div>
         <div className="mr-10">
           <ul className="flex items-center gap-[4vw] font-semibold">
